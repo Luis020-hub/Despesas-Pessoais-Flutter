f1e831276aad95a717e6f7971fe7e1d5d6dd03af
Luis020-hub <guhmonteiro10@hotmail.com>
2023-01-14 22:53:21 -0300

    14/01/2023
    -ListView adicionado
    -Teclado numérico adicionado no campo Value

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -8,6 +8,17 @@ class TransactionForm extends StatelessWidget {
 
   TransactionForm(this.onSubmit, {Key? key}) : super(key: key);
 
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0.0;
+
+    if (title.isEmpty || value <= 0) {
+      return;
+    }
+
+    onSubmit(title, value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -18,12 +29,15 @@ class TransactionForm extends StatelessWidget {
           children: <Widget>[
             TextField(
               controller: titleController,
+              onSubmitted: (_) => _submitForm(),
               decoration: const InputDecoration(
                 labelText: 'Title',
               ),
             ),
             TextField(
               controller: valueController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => _submitForm(),
               decoration: const InputDecoration(
                 labelText: 'Value (R\$)',
               ),
@@ -32,17 +46,15 @@ class TransactionForm extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 TextButton(
-                  onPressed: () {
-                    final title = titleController.text;
-                    final value = double.tryParse(valueController.text) ?? 0.0;
-                    onSubmit(title, value);
-                  },
-                  child: const Text('New Transaction',
-                      style: TextStyle(
-                        color: Colors.purple,
-                        backgroundColor: Colors.transparent,
-                        fontSize: 16,
-                      )),
+                  onPressed: (_submitForm),
+                  child: const Text(
+                    'New Transaction',
+                    style: TextStyle(
+                      color: Colors.purple,
+                      backgroundColor: Colors.transparent,
+                      fontSize: 16,
+                    ),
+                  ),
                 ),
               ],
             )
--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -9,56 +9,57 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tr) {
-            return Card(
-              child: Row(children: [
-                Container(
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 15,
-                    vertical: 10,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 1,
-                    ),
-                  ),
-                  padding: const EdgeInsets.all(10),
-                  child: Text(
-                    'R\$ ${tr.value.toStringAsFixed(2)}',
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                      color: Colors.purple,
-                    ),
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (ctx, index) {
+          final tr = transactions[index];
+          return Card(
+            child: Row(
+              children: [
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 10,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2,
                   ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      tr.title,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  'R\$ ${tr.value.toStringAsFixed(2)}',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    tr.title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
                     ),
-                    Text(
-                      DateFormat.yMMMd().format(tr.date),
-                      style: const TextStyle(
-                        color: Colors.blueGrey,
-                      ),
+                  ),
+                  Text(
+                    DateFormat.yMMMd().format(tr.date),
+                    style: const TextStyle(
+                      color: Colors.blueGrey,
                     ),
-                  ],
-                )
-              ]),
-            );
-          }).toList(),
-        ),
+                  ),
+                ],
+              )
+            ]),
+          );
+        },
       ),
     );
   }
