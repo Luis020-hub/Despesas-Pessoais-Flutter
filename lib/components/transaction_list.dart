652e33cce6026b5716a1ce397cb0533de01ec637
Luis020-hub <guhmonteiro10@hotmail.com>
2023-01-21 21:59:41 -0300

    21/01/2023
    - Finalizando a bar que tem os valores gastos semanalmente.
    - Rework na exibição dos preços em lista.

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,7 +10,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300,
+      height: 440,
       child: transactions.isEmpty
           ? Column(
               children: [
@@ -21,7 +21,6 @@ class TransactionList extends StatelessWidget {
                 ),
                 const SizedBox(height: 40),
                 SizedBox(
-                  height: 200,
                   child: Image.asset(
                     'assets/images/waiting.png',
                     fit: BoxFit.cover,
@@ -34,50 +33,35 @@ class TransactionList extends StatelessWidget {
               itemBuilder: (ctx, index) {
                 final tr = transactions[index];
                 return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context).colorScheme.primary,
-                            width: 2,
-                          ),
-                        ),
-                        padding: const EdgeInsets.all(10),
-                        child: Text(
-                          'R\$ ${tr.value.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                            fontFamily: 'BarlowCondensed',
-                            color: Theme.of(context).colorScheme.primary,
+                  elevation: 5,
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text(
+                            'R\$${tr.value}',
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            tr.title,
-                            style: Theme.of(context).textTheme.headline6,
-                            // style: const TextStyle(
-                            //   fontWeight: FontWeight.bold,
-                            //   fontSize: 16,
-                            // ),
-                          ),
-                          Text(
-                            DateFormat.yMMMd().format(tr.date),
-                            style: const TextStyle(
-                              color: Colors.blueGrey,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
+                    ),
+                    title: Text(
+                      tr.title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat(' d MM y').format(tr.date),
+                    ),
                   ),
                 );
               },
